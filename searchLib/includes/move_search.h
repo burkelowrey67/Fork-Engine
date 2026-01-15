aa0c7d28c80abd78b705ebf9a895517a5d4973e2
burkelowrey67 <burkelowrey67@gmail.com>
2026-01-15 14:53:55 -0700

    Define(searchLib): define entry

--- searchLib/includes/move_search.h
+++ searchLib/includes/move_search.h
@@ -4,21 +4,14 @@
 #include <position/position.h>
 
 
-namespace move::search {
+namespace move {
 
 	/**
 	 * @brief Searches the current position and selects the best move.
-	 *
-	 * Performs a move search on the given chess position using the engine's
-	 * evaluation and search algorithms (e.g., minimax, alpha-beta pruning).
-	 * The position is analyzed from the side to move, and the most favorable
-	 * legal move is returned.
-	 *
 	 * @param position: Reference to the current chess position to be searched.
 	 *                 The position may be temporarily modified during the search
 	 *                 but will be restored before the function returns.
-	 *
 	 * @return move
 	 */
-	move::Move search_moves(chess::Position& position);
+	move::Move search(chess::Position& position);
 }
---
+++ searchLib/src/searchLib.cpp
@@ -0,0 +1,33 @@
+// searchLib.cpp : Defines the functions for the static library.
+
+
+#include "pch.h"
+#include <move_search.h>
+#include <move/move_generation.h>
+#include <move/move_application.h>
+#include <eval.h>
+#include <vector>
+#include <cfloat>
+
+namespace move {
+
+
+	move::Move search(chess::Position& position) {
+		std::vector<uint32_t> moves;
+		moves.reserve(256);
+
+		move::generation::generate_pseudolegal_moves(position, moves);
+		
+		move::Move bestMove;
+		double bestScore = DBL_MIN;
+
+		for (move::Move move : moves) {
+			chess::Position nextPosition = move::move_application::next_position(position, move);
+			double score = position::evaluation::eval(nextPosition);
+			if (score > bestScore) bestMove = move; bestScore = score;
+		}
+
+		return bestMove;
+	}
+}
+
