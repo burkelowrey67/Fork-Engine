6faeede46ce193e7103a45d0b8e4e811cf8dde86
burkelowrey67 <burkelowrey67@gmail.com>
2026-01-15 13:46:33 -0700

    Fix(SearchLib/evalLib): Corrected pch issue
    pch's were not set to generate for some reason. I suspect when I organized things, something may have gone wrong.

--- evalLib/includes/eval.h
+++ evalLib/includes/eval.h
@@ -2,7 +2,7 @@
 #include <position/position.h>
 #include <cmath>
 
-namespace evaluation {
+namespace position::evaluation {
 
 	constexpr double pawnValue = 1;
 	constexpr double knightValue = 3;
@@ -11,5 +11,11 @@ namespace evaluation {
 	constexpr double queenValue = 9;
 	constexpr double kingValue = INT32_MAX;
 
-	double eval(chess::Position position);
+	/*
+	* @brief Returns a double representing the strength of the position for the white player.
+	* Higher values are better, and zero represents an equal position.
+	* @param position: Chess position.
+	* @return double
+	*/
+	double eval(chess::Position& position);
 }
