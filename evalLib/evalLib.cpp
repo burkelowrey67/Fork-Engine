ef232c9fc30e720ff5308ad39ce1e1e1239e3a12
burkelowrey67 <burkelowrey67@gmail.com>
2026-01-15 14:53:26 -0700

    Define(evalLib): define entry

---
+++ evalLib/evalLib.cpp
@@ -0,0 +1,16 @@
+// evalLib.cpp : Defines the functions for the static library.
+
+#include "pch.h"
+#include <eval.h>
+
+namespace position::evaluation {
+
+	double eval(chess::Position& position) {
+		return
+			pawnValue	* position.pawn_diff() +
+			knightValue * position.knight_diff() +
+			bishopValue * position.bishop_diff() +
+			rookValue	* position.rook_diff() +
+			queenValue	* position.queen_diff();
+	}
+}
