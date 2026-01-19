24300fc36df8e112e68177899346f2ea2837117a
burkelowrey67 <burkelowrey67@gmail.com>
2026-01-18 18:52:03 -0700

    Feature(uciLib): add FEN string parsing and formatting

---
+++ uciLib/includes/fen.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <position/position.h>
+#include <string>
+#include <unordered_map>
+
+namespace uci::fen {
+
+	/**
+	 * @brief Parses a FEN string and constructs a chess position.
+	 *
+	 * Converts a Forsyth–Edwards Notation (FEN) string into a
+	 * @ref core::Position object representing the same board state,
+	 * including piece placement, side to move, castling rights,
+	 * en passant target square, halfmove clock, and fullmove number.
+	 * 
+	 * Note: Behavior is undefined if the FEN string is malformed.
+	 *
+	 * @param fen: A valid FEN string describing a chess position.
+	 * @return core::Position The position represented by the FEN string.
+	 *	
+	 */
+	core::Position* parse(const char* fen);
+
+	/**
+	 * @brief Formats a chess position as a FEN string.
+	 *
+	 * Serializes the given @ref core::Position into a valid
+	 * Forsyth–Edwards Notation (FEN) string that fully describes
+	 * the current game state.
+	 *
+	 * @param position The chess position to be formatted.
+	 *
+	 * @return std::string A FEN string representing the position.
+	 */
+	std::string format(core::Position& position);
+}
---
+++ uciLib/src/Fen.cpp
@@ -0,0 +1,146 @@
+#include <fen.h>
+#include <position/bit_board.h>
+#include <square.h>
+#include <regex>
+#include <vector>
+#include <format>
+
+namespace uci::fen {
+
+    static const std::regex fenRegex(
+        R"(^([pnbrqkPNBRQK1-8]+\/){7}([pnbrqkPNBRQK1-8]+) [wb] [-KQkq]{0,4} [a-h36\-] \d+ \d+$)"
+    );
+
+	static constexpr int char_to_index(const char c) {
+		switch (c) {
+		case 'P': return 0; case 'N': return 1; case 'B': return 2;
+		case 'R': return 3; case 'Q': return 4; case 'K': return 5;
+		case 'p': return 6; case 'n': return 7; case 'b': return 8;
+		case 'r': return 9; case 'q': return 10; case 'k': return 11;
+		default:  return -1;
+		}
+	}
+
+    static constexpr char index_to_char(const unsigned int index) {
+        switch (index) {
+        case 0:  return 'P'; case 1:  return 'N'; case 2:  return 'B';
+        case 3:  return 'R'; case 4:  return 'Q'; case 5:  return 'K';
+        case 6:  return 'p'; case 7:  return 'n'; case 8:  return 'b';
+        case 9:  return 'r'; case 10: return 'q'; case 11: return 'k';
+        default: return '?'; // error indicator
+        }
+    }
+
+    static void parse_position_string(const char* posString, core::Position* position) {
+
+        int rank = 0; int file = 0;
+        int squareIndex = 0;
+
+        while (*posString != '\0') {
+            const char c = *posString;
+            posString++;
+
+            if (c == '/') {
+                rank++;
+                file = 0;
+                continue;
+            }
+
+            if (std::isdigit(c)) {
+                file += c - '0';  // Skip that many empty squares
+            }
+            else {
+                int index = char_to_index(c);
+                squareIndex = (7 - rank) * 8 + file;  // Map FEN pos to bitboard index
+                (*position).bitBoards[char_to_index(c)] |= core::bit_board::square_to_bit_board(squareIndex);
+                file++;
+            }
+        }
+
+        if (squareIndex != 7) throw std::invalid_argument("Invalid FEN string"); // Square index must end on h1
+    }
+
+    static void parse_castling(const char* castleString, core::Position* position) {
+        (*position).w_kingSideCastle =  false;
+        (*position).b_kingSideCastle =  false;
+        (*position).w_queenSideCastle = false;
+        (*position).b_queenSideCastle = false;
+
+        for (int i = 0; i < 4; i++) {
+            switch (*(castleString + i))
+            {
+            case 'K': (*position).w_kingSideCastle = true;
+            case 'Q': (*position).w_queenSideCastle = true;
+            case 'k': (*position).b_kingSideCastle = true;
+            case 'q': (*position).b_queenSideCastle = true;
+            default:  return;
+            }
+        }
+    }
+
+    static void parse_en_passant_square(char* sq, core::Position* position) {
+        if (*sq == '-') return;
+        int squareIndex = uci::algebraic_to_square_index(std::string(sq, 2));
+        (*position).enPassantSquare = squareIndex;
+    }
+
+	core::Position* parse(char* fen) {
+        if (!std::regex_match(fen, fenRegex)) throw std::invalid_argument("Invalid FEN string");
+
+        core::Position* position = new core::Position();
+
+        std::vector<char*> spaces;
+        spaces.reserve(6);
+
+        std::vector<char*> words;
+
+        char* token = std::strtok(fen, " "); // split by space
+        while (token != nullptr) {
+            words.push_back(token);
+            token = std::strtok(nullptr, " ");
+        }
+
+        parse_position_string(words[0], position);
+        (*position).toMove = *words[1] == 'w' ? core::Color::White : core::Color::Black;
+        parse_castling(words[2], position);
+        parse_en_passant_square(words[3], position);
+        (*position).fullMoveClock = *words[4] - '0';
+        (*position).halfMoveClock = *words[5] - '0';
+
+        return position;
+	}
+
+    static void format_position(std::string& fen, core::Position& position) {
+
+        for (int rank = 7; rank >= 0; rank--) {
+            int emptyFor = 0;
+
+            for (int file = 0; file < 7; file++) {
+                int squareIndex = file + rank * 8;
+                int pieceAtSquare = position.pieceIndexAtSquare[squareIndex];
+
+                if (pieceAtSquare == -1) emptyFor++;
+                else {
+                    char piece = index_to_char(pieceAtSquare);
+                    fen += emptyFor == 0 ? piece + "" : std::format("%d%c", emptyFor, piece);
+                }
+            }
+
+            if (rank != 0) fen += emptyFor == 0 ? "/" : std::format("%d/", emptyFor + 1);;
+        }
+
+        fen += ' ';
+    }
+
+    std::string format(core::Position& position) {
+        std::string fen;
+        format_position(fen, position);
+        fen += position.toMove == core::Color::White ? 'w' + ' ' : 'b' + ' ';
+        if (position.w_kingSideCastle)  fen += 'K';
+        if (position.w_queenSideCastle) fen += 'Q';
+        if (position.b_kingSideCastle)  fen += 'k';
+        if (position.b_queenSideCastle) fen += 'q';
+        fen += position.halfMoveClock + ' ';
+        fen += position.fullMoveClock;
+    }
+}
