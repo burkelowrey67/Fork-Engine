e62e762f31bcff22d080a9f2be0bff9cc257f1c7
burkelowrey67 <burkelowrey67@gmail.com>
2026-01-20 20:04:27 -0700

    Change(uciLib): change default_position() to return by value
    - Changed Position::default_position() to return by value instead of by pointer.

--- coreLib/includes/position/position.h
+++ coreLib/includes/position/position.h
@@ -34,7 +34,7 @@ namespace core {
 			int fullMoveClock, int halfMoveClock
 		);
 
-		static Position* default_position();
+		static Position default_position();
 
 		void update_masks();
 		void update_lookup();
