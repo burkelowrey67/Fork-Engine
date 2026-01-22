fa7993dfc6272a9096f40539a433f8c6a7b0a89f
burkelowrey67 <burkelowrey67@gmail.com>
2026-01-22 13:51:33 -0700

    Implementation(uciLib): implement UCI command handles
    Implemented base UCI command handles:
    uci, isready, setoption (does not do anything right now), ucinewgame, position, go, stop, quit.

---
+++ uciLib/includes/uci_handles.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <move_search.h>
+
+namespace uci {
+	
+	void handle_uci();
+	
+	void handle_isready();
+
+	void handle_setoption(search::Search& search, std::string& options);
+
+	void handle_ucinewgame(search::Search& search);
+
+	void handle_position(core::Position& out, const std::string& params);
+
+	void handle_go(search::Search& search, core::Position& position, const std::string& params);
+
+	void handle_stop(search::Search& search);
+
+	void handle_quit(search::Search& search);
+}
---
+++ uciLib/src/UCIHandles.cpp
@@ -0,0 +1,58 @@
+#include "pch.h"
+#include <uci_handles.h>
+#include <iostream>
+#include <position/position.h>
+#include <uci.h>
+#include <optional>
+#include <move_conversion.h>
+
+namespace uci {
+
+	void handle_uci() {
+		std::cout <<
+			"id name ForkEngine\n" <<
+			"id author Burke Lowrey\n" <<
+			"uciok" <<
+			std::flush;
+	}
+
+	void handle_isready() {
+		std::cout << "isreadyok" << std::flush;
+	}
+
+	void handle_setoption(search::Search& search, std::string& options) {
+
+	}
+
+	void handle_ucinewgame(search::Search& search) {
+		search.stop();
+		search.reset();
+	}
+
+	void handle_position(core::Position& out, const std::string& params) {
+		if (auto position = parse_position_cmd(params.data())) out = *position;
+	}
+
+	void handle_go(search::Search& search, core::Position& position, const std::string& params) {
+		search::SearchLimits searchLimits = search::SearchLimits();
+		parse_go_cmd(searchLimits, params.data());
+		search.go(position, searchLimits);
+	}
+
+	void handle_stop(search::Search& search) {
+		search.stop();
+		if (std::optional<move::Move> bestMove = search.get_info().bestMove; bestMove.has_value()) {
+			if (std::optional<std::string> moveStr = uci::format_uci_move(*bestMove); moveStr.has_value()) {
+				std::cout << "bestmove " << *moveStr << std::flush;
+			}
+			else {
+				std::cout << "bestmove " << 0000 << std::flush;
+			}
+		}
+	}
+
+	void handle_quit(search::Search& search) {
+		search.stop();
+		exit(0);
+	}
+}
