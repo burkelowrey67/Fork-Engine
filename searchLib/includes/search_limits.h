d47d4a539394a39997bb1da44e8ac5c570c4a4b1
burkelowrey67 <burkelowrey67@gmail.com>
2026-01-21 17:37:50 -0700

    Change(searchLib): require std::optional for SearchLimits' members

--- searchLib/includes/search_limits.h
+++ searchLib/includes/search_limits.h
@@ -1,15 +1,16 @@
 #pragma once
 #include <cmath>
+#include <optional>
 
 namespace search {
 	struct SearchLimits {
-        int depth = -1;
-        int movetime = -1;
-        int wtime = -1;
-        int btime = -1;
-        int winc = 0;
-        int binc = 0;
-        int movestogo = -1;
+        std::optional<int> depth = std::nullopt;
+        std::optional<int> movetime = std::nullopt;
+        std::optional<int> wtime = std::nullopt;
+        std::optional<int> btime = std::nullopt;
+        std::optional<int> winc = std::nullopt;
+        std::optional<int> binc = std::nullopt;
+        std::optional<int> movestogo = std::nullopt;
         int nodes = INT_MAX;
         bool infinite = false;
 	};
